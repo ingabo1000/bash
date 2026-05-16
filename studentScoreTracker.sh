d137453ae5b03d9081bbdcc11fcaa9db063a6afc
ngabo <ngaboizo2000@gmail.com>
2026-05-16 12:29:22 +0200

    simple score tracker improved

--- studentScoreTracker.sh
+++ studentScoreTracker.sh
@@ -4,6 +4,7 @@ declare -a students
 declare -A scores
 highest_score=0
 total_score=0
+average_score=0
 
 for((i=0; i<=2; i++))
 do
@@ -14,18 +15,16 @@ do
   scores["$student_name"]=$score
 
   ((total_score+=$score))
+  ((average_score=total_score / ${#students[@]}))
 
 if [ $score -gt $highest_score ]
 then
   highest_score=$score
-  echo "hightest_score = $highest_score"
 fi
-
-echo "total score = ${total_score}"
 done
 
+  echo "hightest_score = $highest_score"
+  echo "total score = ${total_score}"
+  echo "Average score = $average_score"
 
 
-for student in "${students[@]}"
-do echo "$student score ${scores[$student]}"
-done
